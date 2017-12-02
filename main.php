8a3f88eaa9d63697acbacfc8946e4dcdb8655aec
Unknown <Kingr4@protonmail.ch>
2017-12-02 18:41:29 +0000

    Added counting basis

--- main.php
+++ main.php
@@ -13,7 +13,7 @@
             <nav>
                <a href="loginpage.php"><img src="Assets/Images/DIT_logocol_reverse2013.png"></img></a>
                <span id="spanNav">
-                  <h1> DIT Libary </h1>
+                  <h1> DITSU Vote Counter </h1>
                </span>
                <ul>
                  <li><a href="register.php">Register</a></li>
@@ -29,19 +29,16 @@
 			<div class="w3-row w3-section">
                <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
                <div class="w3-rest">
-                  <input class="w3-input w3-border" type="username" name="username" required placeholder="Candidate 1"><span class="error"><span><?php echo $nameErr;?></span>
+                  <input class="w3-input w3-border" type="username" name="Candidate1" required placeholder="Candidate 1"><span class="error"><span>
                </div>
             </div>
 
 			<div class="w3-row w3-section">
                <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
                <div class="w3-rest">
-                  <input class="w3-input w3-border" type="username" name="username" required placeholder="Candidate 2"><span class="error"><span><?php echo $nameErr;?></span>
+                  <input class="w3-input w3-border" type="username" name="Candidate2" required placeholder="Candidate 2">
                </div>
             </div>
-			
-			
-			
 			
             <input type="submit" value="Submit" class="w3-button w3-block w3-section w3-blue w3-ripple w3-padding">
          </form>
