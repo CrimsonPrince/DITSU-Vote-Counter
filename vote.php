cfca08a3e27f439794afbc85380827e20f74739b
CrimsonPrince <Kingr4@protonmail.ch>
2018-04-02 23:46:31 +0000

    Updated Project Structure, adding .htaccess files and securing for apache

---
+++ vote.php
@@ -0,0 +1,14 @@
+<?php
+   $First = $_POST["First"];
+    if(isset($_POST["Second"];))
+    {
+      $Second = $_POST["Second"];
+    }
+    if(isset($_POST["Third"];))
+    {
+      $Third = $_POST["Third"];
+    }
+
+
+
+ ?>
