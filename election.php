eaeb2b67074524ec53db92b9e2f913f2cd1e92ee
CrimsonPrince <Kingr4@protonmail.ch>
2018-04-23 14:31:48 +0000

    Elections page improved most of candidates display implemented

--- election.php
+++ election.php
@@ -29,7 +29,7 @@ class Election {
     $this->createDatabaseConnection();
     if($this->verifyUser())
     {
-
+      $this->displaycandidates();
     }
     else {
       header('location:main.php');
@@ -62,7 +62,7 @@ class Election {
   private function verifyUser()
   {
 
-    $sql = 'SELECT name, description, id, campus, image_path, longdesc
+    $sql = 'SELECT campus
             FROM election
             WHERE id = :id';
     $query = $this->db_connection->prepare($sql);
@@ -76,6 +76,55 @@ class Election {
 
     }
     return false;
+  }
+
+  private function displaycandidates()
+  {
+    $sql = 'SELECT name, id, image_path, manifesto
+            FROM candidates
+            WHERE id = :id';
+    $query = $this->db_connection->prepare($sql);
+    $query->bindValue(':id', $_GET['election']);
+    $query->execute();
+    print_r($this->db_connection->errorInfo());
+    $results = $query->fetchObject();
+    if($results->campus === $_SESSION['user_campus'] || $results->campus === "all" )
+    {
+      return true;
+
+      include("top.php");
+      echo '<div class="container row">';
+      foreach($results as $result)
+      {
+          echo '<div class="col s12 m4">';
+              echo '<div class="card sticky-action" style="overflow: visible;">';
+                echo '<div class="card-image waves-effect waves-block waves-light">';
+                  echo '<img class="activator" src=' . $result['image_path'] . " style='height:25vh;' >";
+                echo '</div>';
+                echo '<div class="card-content">';
+                  echo '<span class="card-title activator grey-text text-darken-4">' . $result['name'] . '<i class="material-icons right">more_vert</i></span>';
+
+                  echo '<p>' . $result['description'] . '</p>';
+              echo '</div>';
+
+                echo '<div class="card-action">';
+                  echo '<a href="election.php?election=' . $result['id'] . '">Vote Now</a>';
+                echo '</div>';
+
+                echo '<div class="card-reveal" style="display: none; transform: translateY(0%);">';
+                  echo '<span class="card-title grey-text text-darken-4">' . $result['name'] . '<i class="material-icons right">close</i></span>';
+                  echo '<p>'. $result['longdesc'] . '</p>';
+                echo '</div>';
+              echo '</div>';
+            echo '</div>';
+    }
+    echo '</div>';
+    include("bottom.php");
+
+    }
+    return false;
+
+
   }
 
 
